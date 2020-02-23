41ff3feba60b2c0a2d122cfaf910a2e5152f607c
Zhenrui Yue <yuezrhb@gmail.com>
2020-02-22 16:27:45 -0800

    add baseline

---
+++ baseline.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torch.nn.utils.rnn import pack_padded_sequence
+
+# TODO why split encode and decode ??????????
+# TODO unsqueeze, reshape -1
+
+
+class CNN(nn.Module):
+    def __init__(self, embedding_size):
+        super(CNN, self).__init__()
+        resnet = models.resnet152(pretrained=True)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features, embedding_size)
+        self.bn = nn.BatchNorm1d(embedding_size, momentum=0.01)
+
+    def forward(self, images):
+        with torch.no_grad():
+            features = self.resnet(images)
+        features = features.reshape(features.size(0), -1)
+        features = self.linear(features)
+        features = self.bn(features)
+
+        return features
+
+
+class RNN(nn.Module):
+    def __init__(self, embedding_size):
+        super(RNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.lstm = nn.LSTM(embedding_size, hidden_size,
+                            num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.max_length = max_length
+
+    def forward(self, features, captions, lengths):
+        embeddings = self.embedding(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
+        hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
+        return outputs
+
+    def sample(self, features, states=None):
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(self.max_length):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            sampled_ids.append(predicted)
+            inputs = self.embedding(predicted)
+            inputs = inputs.unsqueeze(1)
+
+        sampled_ids = torch.stack(sampled_ids, 1)
+
+        return sampled_ids
